083c8890afe307a8c294f5f15f272bdcad5eba45
Titova Anastasiia <37349944+TitovaA@users.noreply.github.com>
2018-04-13 21:12:05 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main(){
+int a,b,c;
+cout<<"Введите стороны треугольника a,b,c";
+cin>>a;
+cin>>b;
+cin>>c;
+(a==b,b==c)?cout<<"Треугольник является равносторонним":cout<<"Треугольник не является равносторонним";
+return 0;
+}
